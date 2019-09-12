31eba61c043b02b5a7fc42b4f382a6680a3ab204
Alfonso Socarras <asocarras10@gmail.com>
2019-09-12 14:34:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length < 1 
+    puts "The line is currently empty."
+  else 
+   message = []
+   i = 0 
+   while i < katz_deli.length do 
+     message << "#{i + 1}. #{katz_deli[i]}"
+     i += 1 
+   end 
+   puts "The line is currently: #{message.join(" ")}"
+  end 
+end
+
